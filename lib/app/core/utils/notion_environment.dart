7cc055d935fc4152aab7e6154c4a0e8f6258247c
Seungpyo Suh <me@sspzoa.io>
2025-11-19 14:19:14 +0900

    chore: add readme & complete stuff

--- lib/app/core/utils/notion_environment.dart
+++ lib/app/core/utils/notion_environment.dart
@@ -1,7 +1,7 @@
 class NotionEnvironment {
   NotionEnvironment._();
 
-  static const String _baseUrl = 'https://notion-proxy.sspyorea.workers.dev';
+  static const String _baseUrl = 'https://notion-proxy.idsjasan.workers.dev';
   static const String _databaseId = '29967f4bfdac8086b468ef3545b3e471';
 
   static String get apiBaseUrl => _baseUrl;
--- lib/app/pages/test/page.dart
+++ lib/app/pages/test/page.dart
@@ -1,42 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../routes/routes.dart';
 import 'controller.dart';
 
 class TestPage extends GetView<TestPageController> {
   const TestPage({super.key});
 
   Widget linkToRoute(String route) {
-    return TextButton(
-      onPressed: () {
-        Get.toNamed(route);
-      },
-      child: Text(route),
-    );
-  }
-
-  Widget linkToRouteWithArgs(
-      String route, String title, Map<String, dynamic> args) {
-    return TextButton(
-      onPressed: () {
-        Get.toNamed(route, arguments: args);
-      },
-      child: Text(title),
-    );
+    return TextButton(onPressed: () => Get.toNamed(route), child: Text(route));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Routes"),
-        centerTitle: true,
-      ),
+      appBar: AppBar(title: const Text('Routes'), centerTitle: true),
       body: ListView(
         physics: const BouncingScrollPhysics(),
+        padding: const EdgeInsets.all(16),
         children: [
-          linkToRoute('/license'),
-          linkToRoute('/'),
+          linkToRoute(Routes.HOME),
+          linkToRoute(Routes.LICENSE),
+          linkToRoute(Routes.ASSET_LOOKUP),
+          linkToRoute(Routes.QUICK_STATUS),
+          linkToRoute(Routes.ASSET_DETAIL),
         ],
       ),
     );
