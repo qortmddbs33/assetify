c870be670335bbe278c4ff8be393a61f0b69b0d9
Seungpyo Suh <me@sspzoa.io>
2025-11-24 11:33:52 +0900

    feat: flutter web frame

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'app/core/theme/inapp/dark.dart';
 import 'app/core/theme/inapp/light.dart';
 import 'app/core/utils/loader.dart';
 import 'app/routes/pages.dart';
@@ -17,6 +16,14 @@ void main() async {
       theme: lightThemeData,
       initialRoute: kReleaseMode ? Routes.HOME : Routes.TEST,
       getPages: AppPages.pages,
+      builder: (context, child) {
+        return Center(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 768),
+            child: child,
+          ),
+        );
+      },
     ),
   );
 }
